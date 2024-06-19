953e6c68f2cd96a2f56116986e7dcbd701fa004d
mkeloo <mkeloo@ufl.edu>
2024-06-19 06:32:31 -0400

    on to steps.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navTriggerBtn = document.querySelector('#nav_trigger_btn');
+const navMenu = document.querySelector('#nav_menu');
+
+// Event Listener
+navTriggerBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('nav-is-open');
+});
+
+// TIMESTAMP: 35:00 STEPS
